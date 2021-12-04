74f8fd3bd9db42267d4857927b772e3c8f93d102
iman.akbar <iman.akbar@mncgroup.comv>
2021-12-04 09:21:18 +0700

    first

---
+++ app.js
@@ -0,0 +1,76 @@
+//jshint esversion:6
+
+const express = require("express");
+const bodyParser = require("body-parser");
+const ejs = require("ejs");
+const mongoose = require("mongoose");
+
+const app = express();
+
+app.set("view engine", "ejs");
+
+app.use(
+  bodyParser.urlencoded({
+    extended: true,
+  })
+);
+app.use(express.static("public"));
+
+mongoose.connect("mongodb://localhost:27017/wikiDB", { useNewUrlParser: true });
+
+const articleSchema = {
+  title: String,
+  content: String,
+};
+
+const Article = mongoose.model("Article", articleSchema);
+
+app
+  .route("/articles")
+  .get((req, res) => {
+    Article.find((err, foundArticles) => {
+      if (!err) {
+        res.send(foundArticles);
+      } else {
+        res.send(err);
+      }
+    });
+  })
+  .post((req, res) => {
+    console.log();
+    console.log();
+
+    const newArticle = new Article({
+      title: req.body.title,
+      content: req.body.content,
+    });
+
+    newArticle.save((err, foundArticles) => {
+      if (!err) {
+        res.send(foundArticles);
+      } else {
+        res.send(err);
+      }
+    });
+  })
+  .delete((req, res) => {
+    Article.deleteMany((err) => {
+      if (!err) {
+        res.send("success delete");
+      } else {
+        res.send(err);
+      }
+    });
+  });
+
+// app.get("/articles", );
+
+// app.post("/articles", );
+
+// app.delete("/articles", )
+
+//TODO
+
+app.listen(3000, function () {
+  console.log("Server started on port 3000");
+});
